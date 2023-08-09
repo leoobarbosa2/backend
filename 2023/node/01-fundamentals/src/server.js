409127b043d6bd3bac5b5d0d4c711bf5465e844b
leoobarbosa2 <leonardo.pb@icloud.com>
2023-08-09 20:52:55 -0300

    feat: creating and listing fake user

--- 2023/node/01-fundamentals/src/server.js
+++ 2023/node/01-fundamentals/src/server.js
@@ -1,7 +1,27 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((request, response) => {
-    return response.end('Hello World!')
+    const { method, url } = request
+    
+    if(method === 'GET' && url === '/users') {
+
+        return response
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if(method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'Leonardo',
+            email: 'leonardo@icloud.com'
+        })
+        return response.end('Criação de usuário')
+    }
+
+    return response.end('Hello Ignite!')
 })
 
 server.listen(3333)
